75568a2f68a835dab78c0e0b4c1bbb7e7b4bcd3d
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-08-13 22:24:44 +0530

    modify part

--- script1.js
+++ script1.js
@@ -1,10 +1,24 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
+document.querySelectorAll(".action").forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("You have booked this event!");
   });
 });
+
+//FAQs
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
